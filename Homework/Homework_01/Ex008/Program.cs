edeb05341a8556e81feefcf2c354f30e10fb3ee1
shenlindao <shenlin@yandex.ru>
2023-02-24 23:56:33 +0500

    hw_ex008_diagram_and_program

--- Homework/Homework_01/Ex008/Program.cs
+++ Homework/Homework_01/Ex008/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Convert.ToInt32 конвертируем введенное число из строки в целое число
+int N;
+int i;
+bool notEven = true;
+Console.WriteLine("Программа выводит чётные числа от 1 до N (максимальное число 100)");
+Console.WriteLine("Введите число N: ");
+N = Convert.ToInt32(Console.ReadLine());
+if (N > 100) {
+    Console.Write("Слишком большое число");
+} else {
+    for (i = 1; i <= N; i++) {
+        if (i % 2 == 0) {
+            Console.Write(i + ", ");
+            notEven = false;
+        }
+    }
+    if (notEven) {
+        Console.Write("Нет чётных чисел");
+    }
+}
+
