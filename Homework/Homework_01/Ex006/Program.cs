bd560c15f98d603840b73cbcf12b06321bd2e38b
shenlindao <shenlin@yandex.ru>
2023-02-24 22:55:38 +0500

    hw_ex006_program

--- Homework/Homework_01/Ex006/Program.cs
+++ Homework/Homework_01/Ex006/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Convert.ToInt32 конвертируем введенное число из строки в целое число
+int N;
+string even = "да";
+string odd = "нет";
+string result;
+Console.WriteLine("Давайте проверим, чётное число или нет");
+Console.WriteLine("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0) {
+    result = even;
+} else {
+    result = odd;
+}
+Console.Write("Ответ: " + result);
