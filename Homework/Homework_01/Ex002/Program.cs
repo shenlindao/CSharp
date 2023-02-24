47a54da39e036635620de6ef42c424df6954811b
shenlindao <shenlin@yandex.ru>
2023-02-24 22:15:38 +0500

    hw_ex002_changes2

--- Homework/Homework_01/Ex002/Program.cs
+++ Homework/Homework_01/Ex002/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Convert.ToInt32 конвертируем введенное число из строки в целое число
 int a, b, max;
 Console.WriteLine("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
+a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
-int b = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
 if (a > b) {
     max = a;
     Console.Write("Наибольшее число: " + max);
--- Homework/Homework_01/Ex004/Program.cs
+++ Homework/Homework_01/Ex004/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Convert.ToInt32 конвертируем введенное число из строки в целое число
+int a, b, c, max;
+Console.WriteLine("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+с = Convert.ToInt32(Console.ReadLine());
+if ((a > b) & (a > c)) {
+    max = a;
+    Console.Write("Наибольшее число: " + max);
+} else if ((b > a) & (b > c)) {
+    max = b;
+    Console.Write("Наибольшее число: " + max);
+} else {
+    max = c;
+    Console.Write("Наибольшее число: " + max);
+}
