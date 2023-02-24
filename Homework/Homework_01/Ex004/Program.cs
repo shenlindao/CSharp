1792fcb4519da6792cfa979b81f3abdf44d97f75
shenlindao <shenlin@yandex.ru>
2023-02-24 22:26:48 +0500

    hw_ex004_diagram_and_program

--- Homework/Homework_01/Ex004/Program.cs
+++ Homework/Homework_01/Ex004/Program.cs
@@ -1,11 +1,16 @@
 ﻿// Convert.ToInt32 конвертируем введенное число из строки в целое число
-int a, b, c, max;
+int a;
+int b;
+int c;
+int max;
+
 Console.WriteLine("Введите первое число: ");
 a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
-с = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
 if ((a > b) & (a > c)) {
     max = a;
     Console.Write("Наибольшее число: " + max);
