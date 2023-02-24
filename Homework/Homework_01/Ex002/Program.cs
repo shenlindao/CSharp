978ca9c346daf33804eac9df3c1793865d6db588
shenlindao <shenlin@yandex.ru>
2023-02-24 21:59:41 +0500

    hw_ex002_program

--- Homework/Homework_01/Ex002/Program.cs
+++ Homework/Homework_01/Ex002/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Convert.ToInt32 конвертируем введенное число из строки в целое число
+int a, b, max;
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) {
+    max = a;
+    Console.Write("Наибольшее число: " + max);
+} else {
+    max = b;
+    Console.Write("Наибольшее число: " + max);
+}
