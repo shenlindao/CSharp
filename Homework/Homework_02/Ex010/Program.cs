5c07e7674ff8c61f465978fc1800f655876d1936
shenlindao <shenlin@yandex.ru>
2023-02-27 17:38:32 +0500

    Homework_02_Ex010_program

--- Homework/Homework_02/Ex010/Program.cs
+++ Homework/Homework_02/Ex010/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNumber (string text) {
+    Console.Write(text);
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+
+string findSecondNumberOfN (int nLength, int number) {
+    if ( nLength == 3 ) {
+        int secondOfN = number / 10 % 10;
+        string result = "Вторая цифра введённого числа: " + secondOfN;
+        return result;
+    } else {
+        string result = "Требуется ввести трёхзначное число!";
+        return result;
+    }
+};
+
+int number = GetNumber("Введите трёхзначное число: ");
+int nLength = number.ToString().Length;
+string output = findSecondNumberOfN(nLength, number);
+Console.WriteLine(output);
