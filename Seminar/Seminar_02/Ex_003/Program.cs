e705f8733d5c55ab209a11868d28b36edb19a538
shenlindao <shenlin@yandex.ru>
2023-02-27 13:32:16 +0500

    Разбор 3 задачи на семинаре

---
+++ Seminar/Seminar_02/Ex_003/Program.cs
@@ -0,0 +1,23 @@
+﻿int GetNumber(string text)
+{
+  Console.Write(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Check(int value)
+{
+  return value % 7 == 0 && value % 23 == 0;
+}
+
+string GetResult(int number, bool checkResult)
+{
+  string temp = String.Empty;
+  if (checkResult) { temp = "YES"; }
+  else { temp = "NO"; }
+  return "Число: " + number + ". Итог: " + temp;
+}
+
+int value = GetNumber("Введите value: ");
+bool flag = Check(value);
+string output = GetResult(value, flag);
+System.Console.WriteLine(output);
