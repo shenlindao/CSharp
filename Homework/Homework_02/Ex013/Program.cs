6aef52ca701b37569238377fd086c39816e376ea
shenlindao <shenlin@yandex.ru>
2023-02-27 19:16:13 +0500

    Homework_02_Ex013_program

---
+++ Homework/Homework_02/Ex013/Program.cs
@@ -0,0 +1,24 @@
+﻿int GetNumber (string text) {
+    Console.Write(text);
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+
+string findThirdNumberOfN (int nLength, int number) {
+    if ( nLength >= 3 ) {
+        while (number > 999) { // Нужна помощь, пробовал ставить while (nLength > 3) - терминал виснет, почему так? пришлось поменять цикл.
+            number = number / 10;
+        }
+        int thirdOfN = number % 10;
+        string result = "Третья цифра введённого числа: " + thirdOfN;
+        return result;
+    } else {
+        string result = "Третьей цифры нет!";
+        return result;
+    }
+};
+
+int number = GetNumber("Введите число: ");
+int nLength = number.ToString().Length;
+string output = findThirdNumberOfN(nLength, number);
+Console.WriteLine(output);
