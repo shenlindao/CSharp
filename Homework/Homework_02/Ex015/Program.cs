4cc02a79af37a66821abd8099e7903784d22ee10
shenlindao <shenlin@yandex.ru>
2023-02-27 19:58:13 +0500

    Homework_02_Ex015_diagram_fix2

--- Homework/Homework_02/Ex015/Program.cs
+++ Homework/Homework_02/Ex015/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GetNumber (string text) {
+    Console.Write(text);
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+
+bool ValidateNumber (int number) {
+    return number >= 1 && number <= 7;
+};
+
+int number = GetNumber("Введите номер дня недели (от 1 до 7): ");
+bool output = ValidateNumber(number);
+Console.WriteLine(output);
