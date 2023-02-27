80ee1074cf0e31383d784ff30798cc9796220253
shenlindao <shenlin@yandex.ru>
2023-02-27 20:31:33 +0500

    Homework_02_Ex015_program

--- Homework/Homework_02/Ex015/Program.cs
+++ Homework/Homework_02/Ex015/Program.cs
@@ -8,6 +8,23 @@ bool ValidateNumber (int number) {
     return number >= 1 && number <= 7;
 };
 
+string CheckWeekend (int number, bool validete) {
+    if (validete) {
+        if (number > 5) {
+            string result = "Да";
+            return result;
+        } else {
+            string result = "Нет";
+            return result;
+        }
+    } else {
+        string result = "Неверно введён номер дня недели";
+        return result;
+    }
+}
+
+Console.WriteLine("Давайте проверим, является ли день выходным");
 int number = GetNumber("Введите номер дня недели (от 1 до 7): ");
-bool output = ValidateNumber(number);
-Console.WriteLine(output);
+bool validete = ValidateNumber(number);
+string weekEnd = CheckWeekend(number, validete);
+Console.WriteLine(weekEnd);
