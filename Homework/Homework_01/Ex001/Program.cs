baf2a47a649be3dfd34423ee9f221639f4313765
shenlindao <shenlin@yandex.ru>
2023-03-01 19:15:14 +0500

    Ex001_program

---
+++ Homework/Homework_01/Ex001/Program.cs
@@ -0,0 +1,25 @@
+﻿int GetNumberA(string textA) {
+    Console.Write(textA);
+    int A = Convert.ToInt32(Console.ReadLine());
+    return A;
+}
+
+int GetNumberB(string textB) {
+    Console.Write(textB);
+    int B = Convert.ToInt32(Console.ReadLine());
+    return B;
+}
+
+bool CheckNumbers(int numberA, int numberB) {
+    return numberB == numberA * numberA ? true : false;
+}
+
+Console.WriteLine("Давайте проверим, является ли первое число квадратом второго");
+int numberA = GetNumberA("Введите первое число: ");
+int numberB = GetNumberB("Введите второе число: ");
+
+if (CheckNumbers(numberA, numberB)) {
+    Console.WriteLine("Первое число является квадратом второго");
+} else {
+    Console.WriteLine("Первое число НЕ является квадратом второго");
+}
