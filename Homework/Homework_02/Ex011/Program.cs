2b9500ae97e6bc52d391b203545b4e46a0496804
shenlindao <shenlin@yandex.ru>
2023-03-01 14:27:01 +0500

    Homework_02_Ex011_program_readme2

---
+++ Homework/Homework_02/Ex011/Program.cs
@@ -0,0 +1,29 @@
+﻿int GetNumber (string text) {
+    Console.Write(text);
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+
+string checkDigits(int number) {
+    bool ValidateNumber(int number) {
+       return number >= 10 && number < 99 ? true : false;
+    }
+    if (ValidateNumber(number)) {
+        int a = number % 10;
+        int b = number / 10 % 10;
+        int biggerDigit (int number) {
+            int bigger;
+            return a < b ? bigger = b : bigger = a;
+        }
+        string result = "Наибольшее число: " + biggerDigit(number);
+        return result;
+    } else {
+        string result = "Было введено число вне заданного диапозона";
+        return result;
+    }
+}
+
+int number = GetNumber("Введите число из отрезка [10, 99]: ");
+if (number < 0) number *= -1;
+string output = checkDigits(number);
+Console.WriteLine(output);
