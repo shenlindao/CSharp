a55df4ace6ef23f3645e769e62eb469acb785b22
shenlindao <shenlin@yandex.ru>
2023-03-02 12:54:26 +0500

    fix_method_GetNumber

---
+++ Seminar/Seminar_03/Ex024/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetNumber(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string GetResult(int N)
+{
+    string res;
+    for (int i = 1; i < N; i++)
+    {
+        int K = i * i * i;
+        string res = Console.WriteLine(K.ToString() + "  i= " + i.ToString());
+    }
+    return res;
+}
+
+int N = GetNumber("Введите конечное число: ");
+Console.WriteLine(GetResult(N));
