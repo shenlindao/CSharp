d500b2ba93243ffb92878f4f30e8c93fa9724e6a
Artem <shenlin@yandex.ru>
2023-03-13 16:35:32 +0300

    Homework05_Ex036

---
+++ Homework/Homework_05/Ex036/Library.cs
@@ -0,0 +1,36 @@
+public static class Library
+{
+    public static int[] MakeArray()
+    {
+        int[] array = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < array.Length; i++)
+            array[i] = rand.Next(100, 999);
+        return array;
+    }
+    public static string PrintArray(int[] array)
+    {
+        string nArray = String.Empty;
+        for (int i = 0; i < array.Length; i++)
+        {
+            nArray = nArray + array[i] + " ";
+        }
+        return nArray;
+    }
+    public static int PositiveNumbers(int[] array)
+    {
+        int positive = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            {
+                positive += 1;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return positive;
+    }
+}
---
+++ Homework/Homework_05/Ex036/Program.cs
@@ -0,0 +1,8 @@
+﻿using static Library;
+
+Console.Clear();
+Console.WriteLine("Данная программа создаёт массив, заполненный случайными положительными трёхзначными числами и показывает количество чётных чисел в массиве.");
+int[] array = MakeArray();
+Console.WriteLine(PrintArray(array));
+int positive = PositiveNumbers(array);
+Console.WriteLine("Количество чётных чисел в массиве = " + positive);
