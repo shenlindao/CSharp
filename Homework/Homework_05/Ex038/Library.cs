57b1653324156e986bfbe77d9e24746fc0254c0c
Artem <shenlin@yandex.ru>
2023-03-13 16:56:09 +0300

    Homework_05_Ex038_lib_&_prog

---
+++ Homework/Homework_05/Ex038/Library.cs
@@ -0,0 +1,36 @@
+public static class Library
+{
+    public static int[] MakeArray()
+    {
+        int[] array = new int[10];
+        Random rand = new Random();
+        for (int i = 0; i < array.Length; i++)
+            array[i] = rand.Next(1, 10);
+        return array;
+    }
+    public static string PrintArray(int[] array)
+    {
+        string nArray = String.Empty;
+        for (int i = 0; i < array.Length; i++)
+        {
+            nArray = nArray + array[i] + " ";
+        }
+        return nArray;
+    }
+    public static int SumOfNumbers(int[] array)
+    {
+        int sum = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 != 0)
+            {
+                sum += array[i];
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return sum;
+    }
+}
--- Homework/Homework_05/Ex038/Program.cs
+++ Homework/Homework_05/Ex038/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Library;
+
+Console.Clear();
+Console.WriteLine("Данная программа создаёт одномерный массив, заполненный случайными числами и находит сумму чисел массива стоящих на нечетной позиции.");
+int[] array = MakeArray();
+Console.WriteLine(PrintArray(array));
+int sum = SumOfNumbers(array);
+Console.WriteLine("Сумма нечётных чисел в массиве = " + sum);
